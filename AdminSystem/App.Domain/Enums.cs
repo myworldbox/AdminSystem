20604bd2f801cca03bc5f11fbf03e95a1dfcca2e
thecyangate <thecyangate@gmail.com>
2025-10-22 18:39:17 +0800

    fix

--- AdminSystem/App.Domain/Enums.cs
+++ AdminSystem/App.Domain/Enums.cs
@@ -1,12 +1,12 @@
-namespace AdminSystem.Models
+ï»¿namespace AdminSystem.Models
 {
     public class Enums
     {
         public enum Category
         {
             VIP,
-            ¤@¯ë,
-            ¶Â¦W³æ
+            ä¸€èˆ¬,
+            é»‘åå–®
         }
     }
 }
