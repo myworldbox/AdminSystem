8d5c0c687676653ed8ced0fd56a9b57fd7682ca4
thecyangate <56016278+myworldbox@users.noreply.github.com>
2025-12-11 00:23:45 +0800

    fix

--- AdminSystem/App.Application/Dtos/SearchDto.cs
+++ AdminSystem/App.Application/Dtos/SearchDto.cs
@@ -10,6 +10,6 @@ namespace AdminSystem.Application.Dtos
         string OrderName = "Id",
         Enums.Order Order = Enums.Order.asc,
         int Page = 1,
-        int PageSize = 10
+        int PageSize = 3
     );
 }
