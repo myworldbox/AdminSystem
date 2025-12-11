676e39e678ed4c94aa410ba9b459c196e2a42561
thecyangate <56016278+myworldbox@users.noreply.github.com>
2025-12-11 21:50:46 +0800

    fix

--- AdminSystem/App.Application/Dtos/PagedResultDto.cs
+++ AdminSystem/App.Application/Dtos/PagedResultDto.cs
@@ -4,10 +4,7 @@ using AdminSystem.Application.ViewModels;
 
 namespace AdminSystem.Application.Dtos;
 
-public class PagedResultDto<T>
+public class PagedResultDto<T> : PaginationDto
 {
-    public List<T> Items { get; set; } = new();
-    public int TotalRecords { get; set; }
-    public SearchDto SearchDto { get; set; } = new();
-    public int TotalPages => SearchDto.PageSize == 0 ? 1 : (int)Math.Ceiling((double)TotalRecords / SearchDto.PageSize);
+    public IReadOnlyList<T>? Items { get; set; }
 }
---
+++ AdminSystem/App.Application/Dtos/PaginationDto.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using AdminSystem.Domain.Entities;
+using AdminSystem.Application.ViewModels;
+
+namespace AdminSystem.Application.Dtos;
+
+public class PaginationDto
+{
+    public int TotalRecords { get; set; }
+    public SearchDto SearchDto { get; set; } = new();
+    public int TotalPages => SearchDto.PageSize == 0 ? 1 : (int)Math.Ceiling((double)TotalRecords / SearchDto.PageSize);
+}
